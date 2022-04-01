53a414ccea6f4022db4cff79d82fd7ed3821ec51
Zhang Rong Zhan <ra45388791@gmail.com>
2022-04-01 09:28:38 +0800

    yt版面功能練習初始化

---
+++ vue練習/youtubePractise/src/main.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav></nav>
+    <header></header>
+    <main></main>
+    <footer></footer>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+
+}
+</script>
+
+<style>
+</style>
