b47052c85b5c1ac616f6cc4ea9604d558db7ed04
Zhang Rong Zhan <ra45388791@gmail.com>
2022-04-17 17:42:10 +0800

    發現效能問題 修改前

--- vue練習/ver-vite-todolist/src/components/calendar/calenderMainPage.vue
+++ vue練習/ver-vite-todolist/src/components/calendar/calenderMainPage.vue
@@ -11,21 +11,17 @@
 
             <div class="dateBox">
                 <ul class="day">
-                    <li v-for="i of 5" class="oldDays" :key="i">
-                        <button>
-                            10
-                        </button>
+
+
+                    <li is="vue:DayBox" v-for="days of beforeMonthDays" v-bind="days" :key="days.id">
                     </li>
-                    <li v-for="i of 30" :key="i">
-                        <button>
-                            12
-                        </button>
+
+                    <li is="vue:DayBox" v-for="days of thisMonthDays" v-bind="days" :key="days.id">
                     </li>
-                    <li v-for="i of 5" class="oldDays" :key="i">
-                        <button>
-                            11
-                        </button>
+
+                    <li is="vue:DayBox" v-for="days of afterMonthDays" v-bind="days" :key="days.id">
                     </li>
+
                 </ul>
             </div>
 
@@ -43,27 +39,153 @@
     </div>
 </template>
 <script>
-import { mapMutations } from "vuex";
+import { mapState, mapMutations } from "vuex";
+import DayBox from "./dayBox.vue";
+
+
 export default {
     data() {
         return {
-            weekdays: [ // 星期幾
-                { text: '一' }, { text: '二' }, { text: '三' }, { text: '四' }, { text: '五' }, { text: '六' }, { text: '日' },
+            weekdays: [
+                { text: "一" },
+                { text: "二" },
+                { text: "三" },
+                { text: "四" },
+                { text: "五" },
+                { text: "六" },
+                { text: "日" },
             ],
             chooseDate: {
-                year: 2022,
-                month: 4
+                year: NaN,
+                month: NaN
             }
-
-        }
+        };
     },
     computed: {
+        ...mapState(["mainArticles"]),
+        //今日年月日
+        today() {
+            const today = new Date;
+            const year = today.getFullYear();
+            const month = today.getMonth() + 1;
+            const day = today.getDate();
+            const date = this.workWithNumbers(month, day);
+
+
+            this.chooseDate.year = year;
+            this.chooseDate.month = month;
+
+            return `${year}-${date.month}-${date.day}`;
+        },
+        //月份控制器參數
         chooseDateRes() {
             if (String(this.chooseDate.month).length === 1) {
-                return `${this.chooseDate.year}-0${this.chooseDate.month}`
-            } else {
-                return `${this.chooseDate.year}-${this.chooseDate.month}`
+                return `${this.chooseDate.year}-0${this.chooseDate.month}`;
             }
+            else {
+                return `${this.chooseDate.year}-${this.chooseDate.month}`;
+            }
+        },
+        //選中月分
+        thisMonthDays: function () {
+            let arrayDays = [];
+
+            //基礎套用class
+            const styles = [];
+
+            //原始年、月參數
+            const originYear = this.chooseDate.year;
+            const originMonth = this.chooseDate.month - 1;
+            // 取得本月最後一天
+            const totalLastDate = new Date(originYear, originMonth + 1, 0);
+            const totalDate = totalLastDate.getDate();
+            //包裝
+            for (let i = 0; i < totalDate; i++) {
+                const thisMonth = this.workWithNumbers(originMonth + 1, i + 1);
+                const id = `${originYear}-${thisMonth.month}-${thisMonth.day}`;
+
+
+                //包裝
+                arrayDays.push({
+                    id: id,
+                    day: i + 1,
+                    styles: styles,
+                    today: this.isToday(id)     //如果日期是今天 設為true 否則 false
+                });
+            }
+            return arrayDays;
+        },
+        //前一個月顯示
+        beforeMonthDays: function () {
+            let arrayDays = [];
+
+            //基礎套用class
+            const styles = ['oldDays']
+
+            //原始年、月參數
+            const originYear = this.chooseDate.year;
+            const originMonth = this.chooseDate.month - 1;
+            // 取得本月星期幾
+            const firstDate = new Date(originYear, originMonth, 1);
+            let firstDayWeek = firstDate.getDay();
+            // 前一個月的最後一天
+            const lastDay = new Date(originYear, originMonth, 0);
+            let oldDaysBefore = lastDay.getDate();
+            if (firstDayWeek === 0) {
+                firstDayWeek = 7; // 如果是禮拜天 星期參數會是0
+                oldDaysBefore -= 5;
+            }
+            else {
+                //前一個月最後一天 - (本月星期x - 2) 
+                oldDaysBefore -= (firstDayWeek - 2);
+            }
+            // 填充空格的上個月天數
+            for (let i = 0; i < firstDayWeek - 1; i++) {
+                //如果原始月份-1 === 0 代表是 12月
+                const before = this.workWithNumbers(originMonth === 0 ? 12 : originMonth, oldDaysBefore);
+                const id = `${originYear}-${before.month}-${before.day}`;
+
+                //包裝
+                arrayDays.push({
+                    id: `before-${id}`,
+                    day: oldDaysBefore,
+                    styles: styles,
+                    today: this.isToday(id)     //如果日期是今天 設為true 否則 false
+                });
+                oldDaysBefore++;
+            }
+            return arrayDays;
+        },
+        //下一個月顯示
+        afterMonthDays: function () {
+            let arrayDays = [];
+
+            //基礎套用class
+            const styles = ['oldDays']
+
+            //原始年、月參數
+            const originYear = this.chooseDate.year;
+            const originMonth = this.chooseDate.month - 1;
+            const oldDaysAfter = 42 - (this.thisMonthDays.length + this.beforeMonthDays.length);
+            for (let i = 0; i < oldDaysAfter; i++) {
+                const after = this.workWithNumbers(originMonth + 2, i + 1);
+                const id = `${originYear}-${after.month}-${after.day}`;
+
+                //包裝
+                arrayDays.push({
+                    id: `after-${id}`,
+                    day: i + 1,
+                    styles: styles,
+                    today: this.isToday(id)     //如果日期是今天 設為true 否則 false
+                });
+
+            }
+            return arrayDays;
+        },
+
+        totalMonth: function () {
+            const totalMonth = this.beforeMonthDays.concat(this.thisMonthDays, this.afterMonthDays);
+            return totalMonth
         }
     },
     methods: {
@@ -71,7 +193,7 @@ export default {
             if (this.chooseDate.month === 1) {
                 this.chooseDate.year -= 1;
                 this.chooseDate.month = 12;
-                return
+                return;
             }
             this.chooseDate.month -= 1;
         },
@@ -79,14 +201,31 @@ export default {
             if (this.chooseDate.month === 12) {
                 this.chooseDate.year += 1;
                 this.chooseDate.month = 1;
-                return
+                return;
             }
             this.chooseDate.month += 1;
-
+        },
+        isToday: function (date) {
+            if (this.today === date) return true;
+            else return false;
+        },
+        workWithNumbers: function (month, day) {
+            //處裡如果數字為個位數在前方+1個0 以配對資料
+            let idMonth = "";
+            let idDay = "";
+            //如果小於10 在數字前 + 一個0
+            if (month + 1 < 10)
+                idMonth = `0${month}`;
+            else
+                idMonth = `${month}`;
+            if (day + 1 < 10)
+                idDay = `0${day}`;
+            else
+                idDay = `${day}`;
+            return { month: idMonth, day: idDay };
         }
-    }
-
-
+    },
+    components: { DayBox }
 }
 </script>
 <style scoped>
@@ -96,9 +235,6 @@ export default {
     box-sizing: border-box;
 }
 
-li {
-    list-style: none;
-}
 
 
 /* 星期區 */
@@ -173,47 +309,6 @@ li {
     width: 100%;
 }
 
-.dateBox li {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: calc(100% / 7);
-
-    height: 3.5rem;
-}
-
-.dateBox button {
-    position: relative;
-    padding: 0;
-
-    width: 2.5rem;
-    height: 2.5rem;
-
-    color: #fff;
-    background: rgba(51, 255, 0, 0);
-
-    font-size: 1.5em;
-    text-align: center;
-    border: solid 3px rgba(167, 255, 255, 0);
-    box-sizing: border-box;
-    border-radius: 30px;
-
-    transition: 0.1s;
-}
-
-.dateBox button:focus {
-    background: rgb(38, 215, 255);
-    border-radius: 30px;
-
-}
-
-/* !過去日期 */
-.dateBox .oldDays div {
-    color: rgba(255, 255, 255, 0.5);
-
-}
-
-/* !未來日期 */
 .control {
     display: flex;
     justify-content: center;
@@ -224,12 +319,9 @@ li {
     background: rgb(19, 178, 241);
 }
 
-@media (min-width: 768px) {
 
-    .dateBox li {
-        height: 3.5rem;
-    }
-}
+
+@media (min-width: 768px) {}
 
 @media (min-width: 1024px) {
     #calenderPager {
@@ -246,9 +338,6 @@ li {
 
     }
 
-    .dateBox li {
-        height: 3.5rem;
-    }
 
     .control {
         margin-bottom: 0;
@@ -256,17 +345,7 @@ li {
     }
 }
 
-@media (min-width: 1440px) {
-    .dateBox li {
-        height: 5rem;
-    }
+@media (min-width: 1440px) {}
 
-}
-
-@media (min-width: 1920px) {
-    .dateBox li {
-        height: 7rem;
-    }
-
-}
+@media (min-width: 1920px) {}
 </style>
