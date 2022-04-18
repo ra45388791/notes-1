cd364649397c870922c3058d1511977b72309ad7
Zhang Rong Zhan <ra45388791@gmail.com>
2022-04-19 01:03:59 +0800

    合併到主線前

--- vue練習/ver-vite-todolist/src/components/calendar/calenderArticleOptions.vue
+++ vue練習/ver-vite-todolist/src/components/calendar/calenderArticleOptions.vue
@@ -2,18 +2,33 @@
     <transition name="">
         <div class="options">
             <div class="optionsBox">
+
                 <button type="button" class="articleOption">
-                    <div>
+                    <div class="title">
                         <h3>
                             標題
                         </h3>
                     </div>
-                    <div>
-                        <div>
-                            是否結案
-                        </div>
+                    <div class="isClose">
+                        <p>
+                            已結案
+                        </p>
                     </div>
                 </button>
+                <button type="button" class="articleOption">
+                    <div class="title">
+                        <h3>
+                            標題
+                        </h3>
+                    </div>
+                    <div class="isClose">
+                        <p>
+                            未結案
+                        </p>
+                    </div>
+                </button>
+
+
             </div>
         </div>
     </transition>
@@ -78,5 +93,16 @@ export default {
     border: 5px solid #00ffff;
     border-radius: 10px;
     box-sizing: border-box;
+}
+
+.title {
+    width: 70%;
+    font-size: 1.25rem;
+    text-align: start;
+}
+
+.isClose {
+
+    width: 30%;
 }
 </style>
